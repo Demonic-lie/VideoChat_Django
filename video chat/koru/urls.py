38d3e0b8904bd8f4eaa4d7ea5f2670fe0f2a3263
Anjali <anjali999kushwaha@gmail.com>
2021-05-22 22:18:59 +0530

    added host.html template

--- video chat/koru/urls.py
+++ video chat/koru/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name= 'koru'
 urlpatterns = [
     path("", views.index, name="index"),
+    path("host/", views.host, name="host"),
 ]
--- video chat/koru/views.py
+++ video chat/koru/views.py
@@ -5,3 +5,5 @@ from django.shortcuts import render
 def index(request):
     return render(request,"koru/index.html")
 
+def host(request):
+    return render(request,"koru/host.html")
