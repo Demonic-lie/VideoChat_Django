c9dbeea9e581b3d013846accb7ee71b3e38c8974
Anjali <anjali999kushwaha@gmail.com>
2021-05-27 16:55:11 +0530

    hostname and meetingname added to currentchats database

--- video chat/koru/models.py
+++ video chat/koru/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class currentchat(models.Model):
+    hostname = models.CharField(max_length = 30)
+    meetingname = models.CharField(max_length = 30)
+    
+    def __str__(self):
+        return f"Host: {self.hostname} and Meeting ID: {self.meetingname}"
--- video chat/koru/views.py
+++ video chat/koru/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 
+from .models import currentchat
+
+from django.http import HttpResponseRedirect
+
+from django.urls import reverse
+
 # Create your views here.
 
 def index(request):
     return render(request,"koru/index.html")
 
 def host(request):
-    return render(request,"koru/host.html")
+    if request.method == "POST":
+        if request.POST.get('name') and request.POST.get('uid'):
+            post = currentchat()
+            post.hostname = request.POST.get('name')
+            post.meetingname = request.POST.get('uid')
+            post.save()
+            #return render(request, "koru/Chatroom with uid ie meeting id in database")
+    return render(request, "koru/host.html")
+
+
 
 def users(request):
+    if request.method == "POST":
+        if request.POST.get('name') and request.POST.get('uid'):
+            name = request.POST.get('name')
+            uid = request.POST.get('uid')
+            chats = currentchat.objects.values_list("meetingname",flat=True)
+            if uid in chats:
+                return render(request,"koru/host.html")
+            else:
+                return render(request,"koru/index.html")
     return render(request,"koru/users.html")
+
